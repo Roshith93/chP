d77f81a796e45bf29426204a0da7f0bb615ce412
Roshith R <r.roshith@gmail.com>
2021-05-13 10:28:41 +0530

    Navbar font aligned

--- src/Service/index.js
+++ src/Service/index.js
@@ -8,8 +8,7 @@ const CHILD_BASE_URL =
   'https://pra--PERSONAL25.my.salesforce.com/services/apexrest/EmployeeResource/'
 
 export const ServiceCalls = () => {
-  const { setAccessToken } = useContext(ChirpContext)
-  console.log(setAccessToken)
+  const { accessToken, setAccessToken } = useContext(ChirpContext)
 
   const getToken = async () => {
     const response = await axios({
@@ -28,23 +27,34 @@ export const ServiceCalls = () => {
     return response
   }
 
+  const getChildDetails = async () => {
+    const response = await axios({
+      url: CHILD_BASE_URL,
+      method: 'post',
+      headers: {
+        Authorization: `Bearer 00D0v000000Doeo!ARkAQMWHfx4HcD48mE0r31MLSinMdU7FzKmhowaMspWa.PS1tq.5C0ofuBoTLVvVpTHw3Dif7b3.hA8eN.SecIq5aYW0gIsJ`,
+      },
+    })
+    return response
+  }
+
   useEffect(() => {
     getToken()
       .then((res) => {
         setAccessToken(res.data.access_token)
-        console.log(res.data.access_token)
       })
       .catch((err) => console.log(err))
   }, [])
+
+  useEffect(() => {
+    getChildDetails()
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((err) => console.log(err))
+
+  })
   return null
 }
 
-export const getChildDetails = async () => {
-  const response = await axios({
-    url: CHILD_BASE_URL,
-    method: 'post',
-    headers: {},
-  })
-  return response
-}
 // https://test.salesforce.com/services/oauth2/token?username=integrationuser@prahs.com.personal25&password=Change123X9WXRSwyLiJ8esYzZfH9YCcSF&grant_type=password&client_id=3MVG97wqanbUM37Ktam8sz6Qni79f7xgjwJIfxp8PpsoBcM8ww5r.qdluKamSHXTHsgewcydRlmbz1oeK5dOw&client_secret=BEEFEAECCCD9A5E344190C64A793D477FA1C60A9BFA09090351FF83D17093F11
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,13 @@ export const NavBar = () => {
   const { accessToken } = useContext(ChirpContext)
   console.log(accessToken)
   return (
-    <Navbar expand='lg' variant='light' bg='light'>
+    <Navbar expand='lg' variant='dark' bg='dark'>
       <Container>
-        <Navbar.Brand href='#'>
-          {accessToken ? accessToken : 'User not logged in'}
-        </Navbar.Brand>
+        <Navbar.Collapse className='justify-content-end'>
+          <Navbar.Text href='#' style={{fontSize: 'larger', fontWeight: 'bold', color: '#fff'}}>
+            {accessToken ? `Username : ${accessToken}` : 'User not logged in'}
+          </Navbar.Text>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
