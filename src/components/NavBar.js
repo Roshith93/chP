bd3bb499b22d332c486809650916318da0a7615b
roshithr <roshith.radhakrishnan@emids.com>
2021-05-17 11:56:22 +0530

    added logo in Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,10 +9,33 @@ export const NavBar = () => {
   console.log(userDetails)
   return (
     <Navbar expand='lg' variant='dark' bg='dark'>
+      <Navbar.Brand
+        href='#home'
+        style={{
+          paddingLeft: '10px',
+          fontSize: 'larger',
+          fontWeight: 'bold',
+          color: '#fff',
+        }}
+      >
+        <img
+          alt=''
+          src='/images/sub_logo.jpg'
+          width='30'
+          height='30'
+          className='d-inline-block align-top'
+        />{' '}
+        Children's Internal Review Panel (ChiRP)
+      </Navbar.Brand>
       <Container>
         <Navbar.Collapse className='justify-content-end'>
-          <Navbar.Text href='#' style={{fontSize: 'larger', fontWeight: 'bold', color: '#fff'}}>
-            {userDetails ? `Logged in as : ${userDetails?.employeeDetails?.fullName}` : 'User not logged in'}
+          <Navbar.Text
+            href='#'
+            style={{ fontSize: 'larger', fontWeight: 'bold', color: '#fff' }}
+          >
+            {userDetails
+              ? `Logged in as : ${userDetails?.employeeDetails?.fullName}`
+              : 'User not logged in'}
           </Navbar.Text>
         </Navbar.Collapse>
       </Container>
