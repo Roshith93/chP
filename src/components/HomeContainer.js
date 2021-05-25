bab708ed58be4d51bd390a3a551c8612c5eff4a7
roshithr <roshith.radhakrishnan@emids.com>
2021-05-25 18:45:00 +0530

    ermoved unwanted codes from Home container

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,6 +1,4 @@
 import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/Col'
-import Form from 'react-bootstrap/Form'
 import { TabMenus } from './TabMenus'
 
 export const HomeContainer = () => {
