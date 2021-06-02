bdac8df2fe6a3397b5b4179b1bfe95de81e9c8a9
roshithr <roshith.radhakrishnan@emids.com>
2021-06-02 18:12:48 +0530

    removed unwanted import

--- src/components/AcceptRegistration.js
+++ src/components/AcceptRegistration.js
@@ -16,7 +16,6 @@ export const AcceptRegistration = () => {
   const {
     isUserAlreadyRegistered,
     addDataToServer,
-    chirpList,
     setIsDataSubmitted,
     checkBoxStatus,
     setCheckBoxStatus,
